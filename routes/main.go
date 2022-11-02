f6ca2f966722e18a9e989771ed1b08acd0fe20c8
henriqueassiss <contato.henriqueassis@outlook.com>
2022-11-02 19:07:18 -0300

    Creating middleware to set content type

--- routes/main.go
+++ routes/main.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/henriqueassiss/cars-api/controllers"
+	"github.com/henriqueassiss/cars-api/middleware"
 )
 
 func Main() {
 	r := mux.NewRouter()
+	r.Use(middleware.SetContentType)
 	r.HandleFunc("/api/cars", controllers.GetCars).Methods("Get")
 	r.HandleFunc("/api/cars", controllers.InsertCar).Methods("Post")
 	r.HandleFunc("/api/cars/{id}", controllers.EditCar).Methods("Put")
